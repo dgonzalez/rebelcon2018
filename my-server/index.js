b3b3d5f43eca729d3a145eced8e54c469c935ea6
David Gonzalez <nodearth@gmail.com>
2018-06-14 11:44:16 +0100

    Server working

---
+++ my-server/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const metaWeather = require('metaweather-demo')
+
+const app = express()
+
+app.get('/api/location/:city',async (req, res) => {
+  console.log(`Gelocating ${req.params.city}`)
+  res.send(await metaWeather(req.params.city))
+})
+
+app.listen(3001, () => {
+  console.log('Server listening at port 3001')
+})
