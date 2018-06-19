bb9f2e413fb8baf2f275f5523779cd811dea52d1
Elexy <alex@seretalabs.com>
2018-06-19 10:38:18 +0200

    add deployment and deploy script

---
+++ redeploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+APIDEPLOYMENT="infrastructure/deployment.yaml"
+CURVERSION=$(cat ${APIDEPLOYMENT}| grep image | awk {'print $2'} | cut -d':' -f 2)
+NEWVERSION=$(($CURVERSION + 1))
+echo "new version: $NEWVERSION"
+sed -i '.original' "s/my-server:[0-9][0-9]*/my-server:${NEWVERSION}/g;" ${APIDEPLOYMENT}
+cat ${APIDEPLOYMENT}| grep image
+docker build -t elexy/my-server:${NEWVERSION} .
+docker push elexy/my-server:${NEWVERSION}
+kubectl apply -f infrastructure/deployment.yaml
