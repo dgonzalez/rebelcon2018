a525ae6eb1cc904d32e32041ef5ef124b547bb68
Elexy <alex@seretalabs.com>
2018-06-22 11:56:28 +0100

    latest fixes

--- redeploy.sh
+++ redeploy.sh
@@ -6,6 +6,6 @@ NEWVERSION=$(($CURVERSION + 1))
 echo "new version: $NEWVERSION"
 sed -i '.original' "s/my-server:[0-9][0-9]*/my-server:${NEWVERSION}/g;" ${APIDEPLOYMENT}
 cat ${APIDEPLOYMENT}| grep image
-docker build -t elexy/my-server:${NEWVERSION} .
+docker build --no-cache -t elexy/my-server:${NEWVERSION} .
 docker push elexy/my-server:${NEWVERSION}
 kubectl apply -f infrastructure/deployment.yaml
